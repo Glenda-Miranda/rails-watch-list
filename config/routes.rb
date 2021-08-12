3d80ccf120b49088e373942937b7500ab87b482a
Glenda Miranda Henry <gcmirandah@gmail.com>
2021-08-12 13:20:53 -0400

    watch list dìa 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
-    resources :movies, only: [:show]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Movie.destroy_all
+puts "iniciando seed"
 Movie.create(title: "Wonder Woman 1984", 
 overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", 
 poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
@@ -18,3 +19,4 @@ poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg
 Movie.create(title: "Ocean's Eight", 
 overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", 
 poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "finalizado"
